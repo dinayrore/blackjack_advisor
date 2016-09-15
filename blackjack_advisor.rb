270318277750e14f1b71b48c6688723608473360
Kristine <kteich88@gmail.com>
2016-09-15 15:04:49 -0400

    Added first prompts for user input

---
+++ blackjack_advisor.rb
@@ -0,0 +1,7 @@
+puts "Greetings! I am willing to help you optimize your gameplay in Blackjack, but I am going to need some input from you first!"
+
+puts "Please enter your first card: "
+first_card = gets.chomp.to_i
+
+puts "Please enter your second card: "
+second_card = gets.chomp.to_i
