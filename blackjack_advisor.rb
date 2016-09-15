0093f403f418c0602be8c8592fdce77873bccb66
Kristine <kteich88@gmail.com>
2016-09-15 16:41:12 -0400

    Deleted variables, Added comments for next steps

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -1,9 +1,10 @@
+# method sums
+# method for comparing
+
 blackjack_hash = {}
 
-K || Q || J = 10
-A = 1 || 11
-
 puts "Greetings! I am willing to help you optimize your gameplay in Blackjack, but I am going to need some input from you first!"
+puts "Please use the number 10 for King, Queen, or Jack card faces, and the letter A for Ace."
 
 puts "Please enter your first card: "
 player_first_card = gets.chomp.to_i
@@ -16,4 +17,8 @@ blackjack_hash["player_cards"] = [player_first_card, player_second_card]
 puts "Please enter dealers card: "
 dealer_card = gets.chomp.to_i
 
-blackjack_hash["dealer_card_arr"] = dealer_card
+blackjack_hash["dealer_card_key"] = dealer_card
+
+
+puts blackjack_hash["player_cards"]
+puts blackjack_hash["dealer_card_key"]
