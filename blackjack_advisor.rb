eaa75ea20553d39a8bb04848911f49a03495f183
Kristine <kteich88@gmail.com>
2016-09-15 16:11:42 -0400

    Added player_cards array within blackjack_hash && set dealer_card to hash

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -1,14 +1,19 @@
+blackjack_hash = {}
+
 K || Q || J = 10
 A = 1 || 11
-
 
 puts "Greetings! I am willing to help you optimize your gameplay in Blackjack, but I am going to need some input from you first!"
 
 puts "Please enter your first card: "
-first_card = gets.chomp.to_i
+player_first_card = gets.chomp.to_i
 
 puts "Please enter your second card: "
-second_card = gets.chomp.to_i
+player_second_card = gets.chomp.to_i
+
+blackjack_hash["player_cards"] = [player_first_card, player_second_card]
 
 puts "Please enter dealers card: "
 dealer_card = gets.chomp.to_i
+
+blackjack_hash["dealer_card_arr"] = dealer_card
