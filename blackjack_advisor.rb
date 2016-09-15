4a7e675179face2abeb23452614f58439c933772
Kristine <kteich88@gmail.com>
2016-09-15 19:53:16 -0400

    Revised else statements after user input, and included elsif

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -7,19 +7,21 @@ end
 
 def compare_values_hard_hand(player, dealer)
   if player.first != 11 && player.last != 11 #you have a hard hand
-    if ((player.first + player.last).between?(5, 7)) && dealer.between?(2, 11)) ||
-       ((player.first + player.last) == 8 && dealer.between?(2, 4)) ||
-       ((player.first + player.last) == 8 && dealer.between?(7, 11)) ||
-       ((player.first + player.last) == 9 && dealer.between?(7, 11)) ||
-       ((player.first + player.last) == 10 && dealer.between?(10, 11)) ||
-       ((player.first + player.last) == 12 && dealer.between?(2, 3)) ||
-       ((player.first + player.last) == 12 && dealer.between?(7, 11)) ||
-       ((player.first + player.last).between?(13, 16)) && dealer.between?(7, 11))
+    if ((player.first + player.last).between?(5, 7)) && (dealer.between?(2, 11)) ||
+       ((player.first + player.last) == 8) && (dealer.between?(2, 4)) ||
+       ((player.first + player.last) == 8) && (dealer.between?(7, 11)) ||
+       ((player.first + player.last) == 9) && (dealer.between?(7, 11)) ||
+       ((player.first + player.last) == 10) && (dealer.between?(10, 11)) ||
+       ((player.first + player.last) == 12) && (dealer.between?(2, 3)) ||
+       ((player.first + player.last) == 12) && (dealer.between?(7, 11)) ||
+       ((player.first + player.last).between?(13, 16)) && (dealer.between?(7, 11))
       puts "You Better Hit!"
+    end
   end
 end
 
 blackjack_hash = {}
+other_card_values = [*"2".."10"]
 
 puts "Greetings! I can help you optimize your gameplay in Blackjack. But I am going to need some input from you first."
 puts "Please use the number value 10 for King, Queen, or Jack card faces, and the letter A for Ace."
@@ -28,18 +30,22 @@ puts "Please enter your first card: "
 player_first_card = gets.chomp
 if player_first_card == "A"
   player_first_card = 11
-elsif player_first_card !=
+elsif other_card_values.include?(player_first_card)
+  player_first_card = player_first_card.to_i
 else
-    player_first_card = player_first_card.to_i
+  puts "Please enter a valid value moron. Try again later."
+  break
 end
 
 puts "Please enter your second card: "
 player_second_card = gets.chomp.to_i
 if player_second_card == "A"
   player_second_card = 11
-elsif
+elsif other_card_values.include?(player_second_card)
+  player_second_card = player_second_card.to_i
 else
-    player_second_card = player_second_card.to_i
+  puts "Please enter a valid value moron. Try again later."
+  break
 end
 
 blackjack_hash["player_cards"] = [player_first_card, player_second_card]
@@ -48,9 +54,11 @@ puts "Please enter dealers card: "
 dealers_card = gets.chomp.to_i
 if dealers_card == "A"
   dealers_card = 11
-elsif
+elsif other_card_values.include?(dealers_card)
+  dealers_card = dealers_card.to_i
 else
-    dealers_card = dealers_card.to_i
+  puts "Please enter a valid value moron. Try again later."
+  break
 end
 
 blackjack_hash["dealers_card"] = dealers_card
